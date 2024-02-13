a03bbfe3e3b6669bb7fdb9e261d9286b064d92a7
Leonardo Pereira <leonardo.dp.pereia1@gmail.com>
2024-02-13 14:10:08 -0300

    classes done

--- classes.py
+++ classes.py
@@ -1,4 +1,19 @@
-jeep = 2
+class cars:
+    def __init__(self, hp, mod, tires, type):
+        self.hp = hp
+        self.mod = mod
+        self.tires = tires
+        if self.hp > "300" and self.mod == "Back traction":
+            self.type = "sedan"
+        else:
+            self.type = "SUV"
+
+jeep = cars("220 Horse power", "4 by 4", "Rali tires", "")
 
 
-print(jeep)
+BMW = cars("320 Horse power", "Back traction", "Race tires", "")
+
+print(BMW.type)
+print(jeep.type)
+
+
